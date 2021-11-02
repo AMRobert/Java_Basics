88fa485dbd7435660ca66d587565e4d0a39eb80e
Anthony Maria Robert D <56819179+AMRobert@users.noreply.github.com>
2021-11-02 22:23:54 +0530

    Create ConcatStrings.java

---
+++ ConcatStrings.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ConcatStrings
+{
+    public static void main(String args[])
+    {
+        Scanner input = new Scanner(System.in);
+        String S1 = input.next();
+        String S2 = input.next();
+        System.out.println(S1+S2);
+    }
+}
